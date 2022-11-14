bc08848aa374ee67c9a7796ea4975c00342fc410
Dorina <acsinoi.dorina@yandex.ru>
2022-11-14 12:54:54 +0300

    example1

---
+++ Example001/Program.cs
@@ -0,0 +1,26 @@
+﻿// Найти максимальное число из девяти чисел.
+
+
+int a1 = 15;
+int b1 = 50;
+int c1 = 34;
+int a2 = 444;
+int b2 = 56;
+int c2 = 40;
+int a3 = 99;
+int b3 = 100;
+int c3 = 77;
+
+int max = a1;
+if (b1>max) max= b1;
+if (c1 > max) max =c1;
+if (a2 >max) max = a2;
+if (b2 > max) max= b2;
+if (c2 > max) max =c2;
+if (a3 >max) max = a3;
+if (b3 > max) max= b3;
+if (c3 > max) max =c3;
+
+Console.WriteLine (max);
+
+
